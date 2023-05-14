41df62d3864dcbe7cf36fb350c378faa56b1c58e
speed1313 <speedtry13@icloud.com>
2023-05-15 00:57:32 +0900

    add DL problem

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::collections::HashSet;
+
+use rand::Rng;
+fn gcd(a: i32, b: i32) -> i32 {
+    if b == 0 {
+        return a;
+    }
+    gcd(b, a % b)
+}
+
+fn main() {
+    // a = r^x mod p, solve for x
+    let p: u64 = 6700417;
+    // let p: u64 = 2147483647;
+    println!("log_2({}) = {}", p, (p as f64).log2());
+    let a = rand::thread_rng().gen_range(1..p);
+    let r = 5;
+    println!("Problem: {}^x = {} (mod {})", r, a, p);
+
+    let mut x = 1;
+    for i in 0..p
+    {
+        x = (x * r) % p;
+        if x == a {
+            println!("{}^{} = {} (mod {})", r, i, a, p);
+            break;
+        }
+    }
+
+}
